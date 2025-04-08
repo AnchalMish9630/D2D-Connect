ace7e3ccb7c270fa3b742c415a787e613b99c0a6
AnchalMish9630 <anchalmish2025@gmail.com>
2025-04-08 23:04:30 +0530

    config for vite

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export const hello = () => {
   return (
-    <div>hello
+    <div><h1 className="text-3xl font-bold underline bg-red">
+    Hello world!
+  </h1>
+
       <input type="text" placeholder="Type here" className="input" />
       <input type="text" placeholder="Type here" className="input" />
       <input type="text" placeholder="Type here" className="input" />
